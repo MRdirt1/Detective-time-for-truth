3318b9e77630a998b26bbe470cf7c695c3118dfa
MRdirt1 <144840872+MRdirt1@users.noreply.github.com>
2024-08-11 17:27:12 +0200

    patch

--- public/script.js
+++ public/script.js
@@ -1,34 +1,50 @@
-const vnData = '/projects/vnengine/VNData.json';
-
-let json, pageNum = 0, currentPage;
-
-async function grabData() {
-  const resp = await fetch(vnData);
-  json = await resp.json();
-  currentPage = Object.keys(json.Scene1.PAGES)[pageNum];
-  initialize(json);
-  handleOptions(json);
-}
-
-function initialize(data) {
-  const $textbox = document.querySelector("#textbox p");
-  const $optionsbox = document.querySelector('#optionsbox');
+const vnData = {
+    "Scene1": {
+      "PAGES": {
+        "Page0": {
+          "PageText": "Welcome to the game!",
+          "Options": {
+            "Option 1": "Page1",
+            "Option 2": "Page2",
+            "Option 3": "Page3"
+          }
+        },
+        "Page1": {
+          "PageText": "You chose option 1!"
+        },
+        "Page2": {
+          "PageText": "You chose option 2!"
+        },
+        "Page3": {
+          "PageText": "You chose option 3!"
+        }
+      }
+    }
+  };
   
-  $textbox.innerText = data.Scene1.PAGES[currentPage].PageText;
+  let pageNum = 0, currentPage;
   
-  if (data.Scene1.PAGES[currentPage].hasOwnProperty('Options')) {
-    const options = data.Scene1.PAGES[currentPage].Options;
-    Object.keys(options).forEach((key) => {
-      const option = document.createElement('div');
-      option.innerText = key;
-      option.addEventListener('click', () => {
-        currentPage = options[key];
-        pageNum = Object.keys(json.Scene1.PAGES).indexOf(currentPage);
-        initialize(json);
+  function initialize() {
+    const $textbox = document.querySelector("#textbox p");
+    const $optionsbox = document.querySelector('#optionsbox');
+    
+    currentPage = Object.keys(vnData.Scene1.PAGES)[pageNum];
+    $textbox.innerText = vnData.Scene1.PAGES[currentPage].PageText;
+    
+    if (vnData.Scene1.PAGES[currentPage].hasOwnProperty('Options')) {
+      const options = vnData.Scene1.PAGES[currentPage].Options;
+      Object.keys(options).forEach((key) => {
+        const option = document.createElement('div');
+        option.innerText = key;
+        option.addEventListener('click', () => {
+          pageNum = Object.keys(vnData.Scene1.PAGES).indexOf(options[key]);
+          initialize();
+        });
+        $optionsbox.appendChild(option);
       });
-      $optionsbox.appendChild(option);
-    });
+    } else {
+      $optionsbox.innerHTML = '';
+    }
   }
-}
-
-grabData();
+  
+  initialize();
